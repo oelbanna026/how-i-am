c9d159723665861cf569aaf539863e1a21c25da3
oelbanna026 <89281320+oelbanna026@users.noreply.github.com>
2026-04-18 02:55:54 +0300

    Update PlayerProfile.js

--- server/models/PlayerProfile.js
+++ server/models/PlayerProfile.js
@@ -6,7 +6,11 @@ const PlayerProfileSchema = new mongoose.Schema(
     deviceId: { type: String, required: true, unique: true, index: true },
     name: { type: String, required: true },
     avatar: { type: Object, required: true },
+    age: { type: Number, required: false, default: null },
+    country: { type: String, required: false, default: null },
     coins: { type: Number, required: true, default: 100 },
+    friends: { type: [String], required: false, default: [] },
+    providers: { type: Object, required: false, default: {} },
     ui: { type: Object, required: false, default: {} },
     blockedUserIds: { type: [String], required: false, default: [] }
   },
